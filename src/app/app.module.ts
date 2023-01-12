ee01820cd5008051b567e49705d6d3359239896c
Tomislav Kolobaric <kolobaric@hotmail.com>
2023-01-12 11:01:06 +0100

    Imported my service and component + [providers]

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,15 +2,18 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { GameviewComponent } from './gameview/gameview.component';
+import { GamelogicService } from './gamelogic.service'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    GameviewComponent
   ],
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [GamelogicService],
   bootstrap: [AppComponent]
 })
 export class AppModule { }
